999a3c70e0bd87c0f2cfe62d8229db635eea1b89
xiaofeng <760811650@qq.com>
2020-08-31 23:20:28 +0800

    响应拦截

--- src/utils/requset.js
+++ src/utils/requset.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import {getToken,getUsername} from './cookies'
+import { message} from 'antd';
 //第一步创建实例
 
 const service = axios.create({
@@ -20,7 +21,13 @@ const service = axios.create({
 
   //第三步 响应拦截
   service.interceptors.response.use(function (response) {
-    return response;
+    let data = response.data
+    if(data.resCode !== 0){
+      message.error(data.message)
+      return Promise.reject(response)
+    }else{
+      return response;
+    }
   }, function (error) {
     return Promise.reject(error);
   });
